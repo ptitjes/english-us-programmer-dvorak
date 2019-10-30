ef6be45fe6744ad1b01c323e567a7ff7e045e652
Didier Villevalois <ptitjes@free.fr>
2019-10-31 00:51:27 +0100

    Initial import

---
+++ app/src/main/java/com/villevalois/pdk/InputDeviceReceiver.kt
@@ -0,0 +1,9 @@
+package com.villevalois.pdk
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+internal class InputDeviceReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {}
+}
